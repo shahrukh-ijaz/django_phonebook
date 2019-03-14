dec170c7c0d71163b322d81d2632ccddfe8e48fd
Shahrukh.Ijaz <shahrukh@A002-00097.arbisoft.com>
2019-03-14 15:53:36 +0500

    test case file replaced

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from contacts.models import Contact, Number, Email, User
 
-# Create your tests here.
+
+class ContactTestCase(TestCase):
+    def setUp(self):
+        Contact.objects.create(first_name="shahrukh", last_name="ijaz", note="mine")
+
+    def test_contact_create(self):
+        """contact could be created are correctly identified"""
+        first_name = Contact.objects.get(first_name="shahrukh")
+        self.assertEqual(first_name.test_contact_create(), 'The contact can be created!')
