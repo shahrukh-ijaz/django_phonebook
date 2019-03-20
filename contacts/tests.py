55453d32d2baa54a1517c40720ab2a8259f0c696
Shahrukh.Ijaz <shahrukh@A002-00097.arbisoft.com>
2019-03-20 18:13:57 +0500

    fixing test cases

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,26 +1,29 @@
 from django.test import TestCase
+
+from contacts.forms import AddContactForm
 from contacts.models import Contact, Number, Email, User
+from django.contrib.auth.models import User
 
 
 class ContactTestCase(TestCase):
-    def setUp(self):
-        Contact.objects.create(first_name="shahrukh", last_name="ijaz", note="mine")
 
-    def test_contact_create(self):
-        contact = Contact.objects.get(first_name="shahrukh")
-        self.assertEqual(contact.test_contact_create(), 'The contact can be created!')
+    def setUp(self):
+        user = User(username='sharukh', password='shahrukh31')
+        user.save()
+        Contact.objects.create(first_name="shahrukh", last_name="ijaz", note="mine",  dob='2019-03-14', user_id=user)
 
     def able_to_delete_contact(self):
         users = Contact.objects.filter(user_id=2).count()
         if users > 0:
-            self.assertEqual(users, 'The contact have contacts able to delete!')
+            self.assertFalse(users, 'The contact have contacts able to delete!')
         else:
             self.assertFalse(users, 'The contact have contacts not able to delete!')
 
     def test_invalid_form(self):
-        w = Contact.objects.create(first_name="shahrukh", last_name="ijaz", note="mine")
-        data = {'first_name': w.first_name, 'last_name': w.last_name, 'note': w.note, }
-        form = Contact(data=data)
+        user = User(username='sharukh9990', password='shahrukh31')
+        user.save()
+        w = Contact.objects.create(first_name="shahrukh", last_name="ijaz", note="mine", dob='2019-03-14', user_id=user)
+        form = AddContactForm()
         self.assertFalse(form.is_valid())
 
 
