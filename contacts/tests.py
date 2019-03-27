c12927b57191d5d8e67d2cd4214169ed4db3c25f
Shahrukh.Ijaz <shahrukh@A002-00097.arbisoft.com>
2019-03-27 12:36:39 +0500

    changes in testcases

--- contacts/tests.py
+++ contacts/tests.py
@@ -19,18 +19,13 @@ class ContactTestCase(TestCase):
 
     def test_invalid_form(self):
         user = User.objects.create(username='sharukh', password='shahrukh31')
-        w = Contact.objects.create(first_name='shahrukh', last_name='ijaz', note='mine', dob='2019-03-14', user_id=user)
         form = AddContactForm()
         self.assertFalse(form.is_valid())
 
 
 class EmailTestCase(TestCase):
 
-    def setUp(self):
-        user = User.objects.create(username='sharrukh9990', password='shahrukh31')
-        contact = Contact.objects.create(first_name='shahrukh', last_name='ijaz', note='mine', dob='2019-03-14', user_id=user)
-
-    def test_successEmail(self):
+    def test_success_email(self):
         user = User.objects.create(username='sharuukh9990', password='shahrukh31')
         contact = Contact.objects.create(first_name='shahrukh', last_name='ijaz', note='mine', dob='2019-03-14', user_id=user)
         email = Email.objects.create(email='shahrukh.ijaz@arbisoft.com', contact_id=contact)
@@ -39,14 +34,9 @@ class EmailTestCase(TestCase):
 
 class NumberTestCase(TestCase):
 
-    def setUp(self):
-        user = User.objects.create(username='sharukhu9990', password='shahrukh31')
-        contact = Contact.objects.create(first_name='shahrukh', last_name='ijaz', note='mine', dob='2019-03-14', user_id=user)
-
-    def test_successNumber(self):
+    def test_success_number(self):
 
         user = User.objects.create(username='sharukhuu9990', password='shahrukh31')
         contact = Contact.objects.create(first_name='shahrukh', last_name='ijaz', note='mine', dob='2019-03-14',
                                          user_id=user)
-        email = Number.objects.create(number='03209503962', contact_id=contact)
         self.assertEqual('Number Created!', 'Number Created!')
