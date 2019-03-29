fbbd16acd234e70dc332299a573da33b5348df20
Shahrukh.Ijaz <shahrukh@A002-00097.arbisoft.com>
2019-03-29 14:46:06 +0500

    mailtrap configuration and requirements.txt added

--- phone_book/settings.py
+++ phone_book/settings.py
@@ -122,9 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shahrukhijaz31@gmail.com'
-EMAIL_HOST_PASSWORD = 'your_pas'
+if DEBUG:
+    EMAIL_HOST = 'smtp.mailtrap.io'
+    EMAIL_HOST_USER = '9ce6c7506ca3e6'
+    EMAIL_HOST_PASSWORD = '8ffbe07823806e'
+    EMAIL_PORT = '2525'
