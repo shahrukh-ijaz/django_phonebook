3ca40eeef3213ebabafb039209f2ce29f7ac110c
Shahrukh.Ijaz <shahrukh@A002-00097.arbisoft.com>
2019-04-22 15:58:47 +0500

    optimization of permission code

--- contacts/permissions.py
+++ contacts/permissions.py
@@ -5,8 +5,4 @@ from contacts.decorators import validate_token
 class IsUserLogin(BasePermission):
     @validate_token
     def has_object_permission(self, request, view, obj):
-
-        if validate_token(request) is not None:
-            return True
-
         return obj.owner == request.user
